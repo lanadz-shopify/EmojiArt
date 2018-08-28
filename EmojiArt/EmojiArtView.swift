0d8fd092907fba6faebb6b9ec50e0f5fa5ca9640
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2018-08-28 22:29:02 +0800

    Add gestures to subviews: resize and move

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -47,6 +47,7 @@ class EmojiArtView: UIView, UIDropInteractionDelegate {
         label.attributedText = attributedString
         label.sizeToFit()
         label.center = point
+        addEmojiArtGestureRecognizers(to: label)
         addSubview(label)
     }
 
